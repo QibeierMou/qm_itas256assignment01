07802badcd8c85a2f9994e774028890400d6f96a
Jason <qibeier.mou@itas.ca>
2026-02-08 18:48:41 -0800

    Initial commit

---
+++ JasonITAS256-A01/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from jasonitas256-a01!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ JasonITAS256-A01/parser.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup
+
+def parse_jobbank_jobs(soup, max_jobs=15):
+    """Parse Job Bank Canada HTML and return list of job dicts."""
+    jobs = []
+
+    job_cards = soup.find_all('article', class_='resultJobItem')[:max_jobs]
+
+    for card in job_cards:
+        title_tag = card.find("a", class_="resultJobItemTitle")
+        company_tag = card.find("div", class_="employerName")
+        location_tag = card.find("div", class_="location")
+
+        job = {
+            "title": title_tag.get_text(strip=True) if title_tag else"",
+            "company": company_tag.get_text(strip=True) if company_tag else"",
+            "location": location_tag.get_text(strip=True) if location_tag else"",
+            "source": "Job Bank Canada"
+        }
+        jobs.append(job)
+    return jobs 
+
+
+def parse_indeed_jobs(soup, max_jobs=15):
+    """Parse Indeed Canada HTML and return list of job dicts."""
+    jobs = []
+
+    job_cards = soup.find_all("div", class_="job_seen_beacon")[:max_jobs]
+
+    for card in job_cards:
+        title_tag = card.find("h2", class_="jobTitle")
+        company_tag = card.find("span", class_="companyName")
+        location_tag = card.find("div", class_="companyLocation")
+
+        job = {
+            "title": title_tag.get_text(strip=True) if title_tag else "",
+            "company": company_tag.get_text(strip=True) if company_tag else "",
+            "location": location_tag.get_text(strip=True) if location_tag else "",
+            "source": "Indeed"
+        }
+
+        jobs.append(job)
+
+    return jobs
